4319799e468783328dfd9647e0d6a97540f1da2e
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-06-30 00:17:46 +0200

    update webworker code

--- webworker.js
+++ webworker.js
@@ -1,24 +1,26 @@
-const MAIN_URL = 'FactorialTask/bin/Debug/bridge/';
+const MAIN_URL = '';
 const requiredScriptNames = [
-  'bridge.js',
-  'bridge.console.js',
-  'bridge.meta.js',
-  'FactorialTask.js',
-  'FactorialTask.meta.js'
+  'runtime.js',
+  'mono.js',
 ];
+
+let App = {
+  init: () => { App.performTask = Module.mono_bind_static_method("[FactorialTask] FactorialTask.FactorialTask:Perform") }
+}
+
 
 try {
   const requiredScripts = requiredScriptNames.map(
     scriptName => MAIN_URL + scriptName
   );
+  console.log('tests')
   importScripts(...requiredScripts);
 } catch (error) {
   console.error('Error while importing scripts', error);
 }
 
 self.onmessage = e => {
-  const task = new FactorialTask.FactorialTask();
-  const result = task.Perform(e.data);
+  const result = App.performTask(e.data);
 
   postMessage(result);
 };
