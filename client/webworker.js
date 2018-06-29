a9a37bb05cc4d6c44882d1419e015b2c461e8058
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-06-30 00:37:09 +0200

    move output files to separate folders

--- client/webworker.js
+++ client/webworker.js
@@ -1,26 +1,21 @@
-const MAIN_URL = '';
 const requiredScriptNames = [
   'runtime.js',
   'mono.js',
 ];
 
-let App = {
-  init: () => { App.performTask = Module.mono_bind_static_method("[FactorialTask] FactorialTask.FactorialTask:Perform") }
-}
-
-
 try {
   const requiredScripts = requiredScriptNames.map(
-    scriptName => MAIN_URL + scriptName
+    scriptName => scriptName
   );
-  console.log('tests')
   importScripts(...requiredScripts);
 } catch (error) {
   console.error('Error while importing scripts', error);
 }
 
 self.onmessage = e => {
-  const result = App.performTask(e.data);
+  const performTask = Module.mono_bind_static_method("[FactorialTask] FactorialTask.FactorialTask:Perform");
+
+  const result = performTask(e.data);
 
   postMessage(result);
 };
