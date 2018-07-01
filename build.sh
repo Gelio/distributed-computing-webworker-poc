88fb22ecc49c3ddc535111de72d61aa1e56e3693
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-07-01 23:01:40 +0200

    check operating system

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 MONO_SDK=$(pwd)/mono-wasm-sdk
 
-dotnet build Inzynierka.sln
+platform="$(uname -s)"
+case "${platform}" in
+    MSYS*)     buildCmd="dotnet build";;
+    *)          buildCmd="xbuild";;
+esac
+
+eval $buildCmd Inzynierka.sln
 mono $MONO_SDK/packager.exe -prefix=FactorialTask/bin/Debug -out=client FactorialTask.dll
