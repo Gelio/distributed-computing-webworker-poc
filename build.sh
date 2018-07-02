2c625dfba8a56a8aaaa19ed406decb43a714d950
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-07-02 08:18:17 +0200

    use dotnet build on MINGW

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@ MONO_SDK=$(pwd)/mono-wasm-sdk
 
 platform="$(uname -s)"
 case "${platform}" in
-    MSYS*)     buildCmd="dotnet build";;
-    *)          buildCmd="xbuild";;
+	MINGW*) ;&
+	MSYS*)     buildCmd="dotnet build";;
+	*)          buildCmd="xbuild";;
 esac
+
 
 eval $buildCmd Inzynierka.sln
 mono $MONO_SDK/packager.exe -prefix=FactorialTask/bin/Debug -out=client FactorialTask.dll
