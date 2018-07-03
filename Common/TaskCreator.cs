4720174851cee861e29123bac8f77e3a13d0b2ce
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-07-03 10:53:18 +0200

    TaskCreator should create classes implementing ISubtask

--- Common/TaskCreator.cs
+++ Common/TaskCreator.cs
@@ -5,15 +5,15 @@ namespace Common
 {
     public static class TaskCreator
     {
-        public static ITask CreateTask(string assemblyName, string className)
+        public static ISubtask CreateTask(string assemblyName, string className)
         {
             var assembly = Assembly.Load(assemblyName);
             var classType = assembly.GetType(className);
 
-            if (classType.GetInterface(nameof(ITask)) == null)
-                throw new ArgumentException($"Class {className} does not implement the {nameof(ITask)} interface");
+            if (classType.GetInterface(nameof(ISubtask)) == null)
+                throw new ArgumentException($"Class {className} does not implement the {nameof(ISubtask)} interface");
 
-            return (ITask)Activator.CreateInstance(classType);
+            return (ISubtask)Activator.CreateInstance(classType);
         }
     }
 }
